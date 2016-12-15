363aed5ed63b190fe93972c9bc120f80da5c871f
JoshLTrask <176345@tldsb.me>
2016-12-15 11:26:51 -0500

    Update Troon.py

--- Troon.py
+++ Troon.py
@@ -1,1 +1,55 @@
+class Main:
 
+def setup:
+    print ("in setup")
+
+def main_loop:
+    print("in main loop")
+
+def sound_effect:
+    print("in sound effect")
+
+    
+class Player: 
+  def init(self,position,direction,colour) :
+     print("in position direction image")
+
+  def update:
+      print("in update")
+
+  def draw:
+      print("in draw")
+
+  def go_left:
+      print("in go left")
+
+  def go_right:
+      print("in go right")
+
+  def go_up:
+      print("in go up")
+
+  def go_down:
+      print("in go down")
+
+  def check_collision:
+      print("in check collision")
+
+  def make_path:
+      print("in make path")
+
+      
+class GameBoard:
+
+  def update:
+      print("in update")
+
+  def draw:
+      print("in draw")
+class Path:
+
+  def draw:
+      print("in draw")
+
+  def makepath:
+      print("in make path")
