185d49c2a7145f378b8677763c1de5c43483c8c9
JoshLTrask <176345@tldsb.me>
2016-12-21 22:24:23 -0500

    Update Troon.py

--- Troon.py
+++ Troon.py
@@ -2,14 +2,10 @@ import pygame, sys, os, copy, time
 from pygame.locals import *
 import pygame as py
 
-
-
-
-#Trask & Heinz
 class Main:
 
     def __init__(self):
-        print('init')
+       # print('init')
         self.main_dir = os.path.split(os.path.abspath(__file__))[0]
         self.setup()
         self.sound_effect()
@@ -25,29 +21,30 @@ class Main:
 
 
     def setup(self):
-        print ("in setup")
+       # print ("in setup")
 
         pygame.init()
 
         self.DISPLAY = DISPLAY
 
 
-        self.board = GameBoard()
+        self.board = board
         self.board.draw(self.DISPLAY)
+
         self.clock = pygame.time.Clock()
 
 
 
 
     def main_loop(self):
-        print("in main loop")
+       # print("in main loop")
 
         while True:
             #60 frames/sec
             self.clock.tick(1000/30)
 
             pygame.display.update()
-            self.board.draw(self.DISPLAY)
+            #self.board.draw(self.DISPLAY)
             for event in pygame.event.get():
                 if event.type==QUIT:
                     pygame.quit()
@@ -60,29 +57,29 @@ class Main:
 
         Key = pygame.key.get_pressed()
 
-        if Key[pygame.K_UP] :
+        if Key[pygame.K_UP] and self.p1.dir != 2:
             self.p1.dir = 0
 
-        if Key[pygame.K_RIGHT]:
+        if Key[pygame.K_RIGHT] and self.p1.dir != 3:
             self.p1.dir = 1
 
-        if Key[pygame.K_LEFT]:
+        if Key[pygame.K_LEFT] and self.p1.dir != 1:
             self.p1.dir = 3
 
-        if Key[pygame.K_DOWN]:
+        if Key[pygame.K_DOWN] and self.p1.dir != 0:
             self.p1.dir = 2
 
         #p2 move
-        if Key[pygame.K_w]:
+        if Key[pygame.K_w] and self.p2.dir != 2:
             self.p2.dir = 0
 
-        if Key[pygame.K_d]:
+        if Key[pygame.K_d] and self.p2.dir != 3:
             self.p2.dir = 1
 
-        if Key[pygame.K_a]:
+        if Key[pygame.K_a] and self.p2.dir != 1:
             self.p2.dir = 3
 
-        if Key[pygame.K_s]:
+        if Key[pygame.K_s] and self.p2.dir != 0:
             self.p2.dir = 2
 
         #move p1
@@ -114,7 +111,7 @@ class Main:
 
 
 
-        #method done by Jakob H.
+
     def sound_effect(self):
         if pygame.mixer:
             music = os.path.join(self.main_dir, 'Music.wav')
@@ -122,30 +119,37 @@ class Main:
             pygame.mixer.music.play(-1)
 
 
-
-
-
-#Class by Couper and Trask
 class Player:
-    def __init__(self,position,direction,colour,dis) :
-        print('initialize player')
+    def __init__(self,position,direction,colour,dis, EEEEE) :
+        #print('initialize player')
         self.lives = 3
         self.pos = position
         self.ogpos = copy.deepcopy(self.pos)
+        self.lastpos = []
+        self.pathnum = 0
         self.dir = direction
+        self.olddir = copy.deepcopy(self.dir)
         self.col = colour
-        self.paths = []
+        self.counter = 0
+        self.paths = [[self.pos[0],self.pos[1],self.pos[0] + 8, self.pos[1]+8]]
+        self.turns = 0
+        self.ID =EEEEE
 
         self.DISPLAY = dis
 
     def update(self):
         #print("in update")
 
+        #paths
 
-        #move player
 
+        print(self.paths)
+        print('pos', self.pos)
 
         self.check_collision()
+        self.olddir = copy.deepcopy(self.dir)
+
+
         self.draw()
 
     def draw(self):
@@ -156,64 +160,136 @@ class Player:
 
 
     def go_left(self):
-        print("in go left")
+        #print("in go left")
         self.pos[0] -= 3
 
+        if self.olddir != self.dir:
+            print('new')
+            self.paths.append([self.pos[0],self.pos[1],self.pos[0] + 8, self.pos[1 ]+8])
+            self.turns += 1
+        else:
+            self.paths[len(self.paths)-1][0] -= 3
+
+
     def go_right(self):
-        print("in go right")
+        #print("in go right")
         self.pos[0] += 3
 
+        if self.olddir != self.dir:
+            print('new')
+            self.paths.append([self.pos[0],self.pos[1],self.pos[0] + 8, self.pos[1]+8])
+            self.turns += 1
+        else:
+            self.paths[len(self.paths)-1][2] += 3
+
     def go_up(self):
-        print("in go up")
+        #print("in go up")
         self.pos[1] -= 3
 
+        if self.olddir != self.dir:
+            print('new')
+            self.paths.append([self.pos[0],self.pos[1],self.pos[0] + 8, self.pos[1 ]+8])
+            self.turns += 1
+        else:
+            self.paths[len(self.paths)-1][1] -= 3
+
     def go_down(self):
-        print("in go down")
+       # print("in go down")
         self.pos[1] += 3
+
+        if self.olddir != self.dir:
+            print('new')
+            self.paths.append([self.pos[0], self.pos[1], self.pos[0] + 8, self.pos[1]+8])
+            self.turns += 1
+        else:
+            self.paths[len(self.paths) - 1][3] += 3
 
 
     def check_collision(self):
-        print("in check collision")
+        # print("in check collision")
 
         if self.pos[0] <= 7:
-            self.kill()
-            self.start_again()
+               #print('wallDeath')
+               self.kill()
+               self.start_again()
         if self.pos[1] <= 7:
-            self.kill()
-            self.start_again()
-        if self.pos[0] >= (800 - 18):
-            self.pos[0] = (800 - 18)
-        if self.pos[1] >= 489:
-            self.pos[1] = 489
+               #print('wallDeath')
+               self.kill()
+               self.start_again()
+        if self.pos[0] >= (800 - 15):
+               #print('wallDeath')
+               self.pos[0] = (800 - 18)
+               self.kill()
+               self.start_again()
 
-    def make_path(self):
-          print("in make path")
+        if self.pos[1] >= 492:
+               #print('wallDeath')
+               self.pos[1] = 489
+               self.kill()
+               self.start_again()
+
+        #path collision
+
+        valid = True
+        if self.ID == 1:
+            for i in range(p1.turns - 1):
+                if valid == True and self.pos[0]+1 >= p1.paths[i][0] and self.pos[0] <= p1.paths[i][2] and self.pos[1] >= p1.paths[i][1] and self.pos[1]+1 <= p1.paths[i][3]:
+                    print('HIT')
+                    valid = False
+                    self.kill()
+                    self.start_again()
+
+            for i in range(p2.turns + 1):
+                if valid == True and self.pos[0]+1 >= p2.paths[i][0] and self.pos[0] <= p2.paths[i][2] and self.pos[1] >= p2.paths[i][1] and self.pos[1]+1 <= p2.paths[i][3]:
+                    print('HIT')
+                    valid = False
+                    self.kill()
+                    self.start_again()
+
+        else:
+            for i in range(p1.turns + 1):
+                if valid == True and self.pos[0]+1 >= p1.paths[i][0] and self.pos[0] <= p1.paths[i][2] and self.pos[1] >= p1.paths[i][1] and self.pos[1]+1 <= p1.paths[i][3]:
+                    print('HIT')
+                    valid = False
+                    self.kill()
+                    self.start_again()
+
+            for i in range(p2.turns - 1):
+                if valid == True and self.pos[0]+1 >= p2.paths[i][0] and self.pos[0] <= p2.paths[i][2] and self.pos[1] >= p2.paths[i][1] and self.pos[1]+1 <= p2.paths[i][3]:
+                    print('HIT')
+                    valid = False
+                    self.kill()
+                    self.start_again()
 
 
-    #method by Couper and Braiden
+
+
+
+
+
+
+
     def kill(self):
-        print('kill player')
+        #print('kill player')
         self.lives -= 1
+
         p1.pos = copy.deepcopy(p1.ogpos)
         p1.dir = 2
         p2.pos = copy.deepcopy(p2.ogpos)
         p2.dir = 0
 
-
-
     def start_again(self):
         time.sleep(1)
+        p1.turns = 0
+        p2.turns = 0
+        p1.paths = [[p1.pos[0],p1.pos[1],p1.pos[0] + 8, p1.pos[1]+8]]
+        p2.paths = [[p2.pos[0],p2.pos[1],p2.pos[0] + 8, p2.pos[1]+8]]
+        p1.lastpos = []
+        p2.lastpos = []
 
-
-
-        if self.pos[1] <= 10:
-            self.pos[1] = 10
-
-        if self.pos[0] >= (800 - 18):
-            self.pos[0] = (800 - 18)
-        if self.pos[1] >= 489:
-            self.pos[1] = 489
-
+        if p1.lives == 0 or p2.lives == 0:
+            sys.exit()
+        board.draw(DISPLAY)
 
 
 
@@ -271,17 +347,14 @@ class GameBoard:
         window.blit(score2, (400, 540))
         window.blit(player2, (400, 510))
 
-class Path:
 
-    def draw(self):
-      print("in draw")
 
-    def makepath(self):
-      print("in make path")
 
 DISPLAY = pygame.display.set_mode((800,600),0,32)
-p1 = Player([100,100],2,(255,150,0),DISPLAY)
-p2 = Player([700,300],0,(0,100,255),DISPLAY)
-
+p1 = Player([99, 99],2,(255,150,0),DISPLAY,1)
+p2 = Player([699,300],0,(0,100,255),DISPLAY,2)
+board = GameBoard()
 
 if __name__ == '__main__': run = Main()
+
+
